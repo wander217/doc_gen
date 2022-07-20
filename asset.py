4b9fae4241369327308c0e5d98588daaf70362c4
thinhtq <Trinhthinh2171999@gmail.com>
2022-07-20 08:49:59 +0700

    initial

---
+++ asset.py
@@ -0,0 +1,21 @@
+from typing import Dict
+
+JSON_DATA_PATH: Dict = {
+    'city_path': r'D:\python_project\doc_gen\data\city.json',
+    'district_path': r'D:\python_project\doc_gen\data\district.json',
+    'vi_name_path': r'D:\python_project\doc_gen\data\vi_company_name.json',
+    'eng_name_path': r'D:\python_project\doc_gen\data\eng_company_name.json',
+    'short_name_path': r'D:\python_project\doc_gen\data\short_company_name.json',
+    'address_path': r'D:\python_project\doc_gen\data\address.json',
+    'phone_path': r'D:\python_project\doc_gen\data\phone.json',
+    'fax_path': r'D:\python_project\doc_gen\data\fax.json',
+    'email_path': r'D:\python_project\doc_gen\data\email.json',
+    'website_path': r'D:\python_project\doc_gen\data\website.json',
+    'business_path': 'D:\python_project\doc_gen\data\job.json',
+    'business_annotation': r'D:\python_project\doc_gen\data\business_annotation.json',
+    'idcard_code_path': r'D:\python_project\doc_gen\data\idcard_code.json',
+    'idcard_place_path': r'D:\python_project\doc_gen\data\idcard_place.json',
+    'persistance_residence_path': r'D:\python_project\doc_gen\data\address.json',
+    'living_place_path': r'D:\python_project\doc_gen\data\address.json',
+    'ethnicity_path': r'D:\python_project\doc_gen\data\people.json'
+}
